9f904eb2164fcbf2243fd1166d533c50b1610a37
Samuel Airisniemi <Samuel.Airisniemi@iths.se>
2026-01-14 11:39:21 +0100

    övning 1

---
+++ Statistik_övning1.py
@@ -0,0 +1,6 @@
+positive_cases = 3
+total_cases = 10
+
+empirical_probability = positive_cases/total_cases
+
+print(f"Empirisk sannorlikhet att byggnaden har asbestproblem: P = {positive_cases}/{total_cases} = {empirical_probability:.2f}%")
