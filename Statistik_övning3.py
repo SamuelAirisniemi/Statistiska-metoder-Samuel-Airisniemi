523a91502435aa60487e04baa70955412626d575
Samuel Airisniemi <Samuel.Airisniemi@iths.se>
2026-01-14 11:50:02 +0100

    övning 3

---
+++ Statistik_övning3.py
@@ -0,0 +1,32 @@
+import math
+
+fak_9 = math.factorial(9)
+fak_6 = math.factorial(6)
+perm_7_3 = math.perm(7, 3)
+perm_6_2 = math.perm(6, 2)
+perm_5_5 = math.perm(5, 5)
+perm_6_6 = math.perm(6, 6)
+comb_9_4 = math.comb(9, 4)
+comb_8_3 = math.comb(8, 3)
+comb_8_5 = math.comb(8, 5)
+comb_8_0 = math.comb(8, 0)
+comb_8_7 = math.comb(8, 7)
+
+print("Fakulteter:")
+print("9! =", fak_9)
+print("6! =", fak_9)
+
+print("\nPermutationer:")
+print("7P3 =", perm_7_3)
+print("6P2 =", perm_6_2)
+print("5P5 =", perm_5_5)
+print("6P6 =", perm_6_6)
+
+print("\nKombinationer:")
+print("9C4 =", comb_9_4)
+print("8C3 =", comb_8_3)
+
+print("\nBinomialkoefficienter:")
+print("C(8,5) =", comb_8_5)
+print("C(8,0) =", comb_8_0)
+print("C(8,7) =", comb_8_7)
