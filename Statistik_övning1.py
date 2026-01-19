fc3db82191d26b94217b02697f1831820950feac
Samuel Airisniemi <Samuel.Airisniemi@iths.se>
2026-01-19 13:43:47 +0100

    fixade småfel

--- Statistik_övning1.py
+++ Statistik_övning1.py
@@ -3,4 +3,4 @@ total_cases = 10
 
 empirical_probability = positive_cases/total_cases
 
-print(f"Empirisk sannorlikhet att byggnaden har asbestproblem: P = {positive_cases}/{total_cases} = {empirical_probability:.2f}%")
+print(f"Empirisk sannorlikhet att byggnaden har asbestproblem: P = {positive_cases}/{total_cases} = {empirical_probability:.2f}")
